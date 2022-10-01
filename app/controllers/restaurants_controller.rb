c531cbf9c7836777b41e1130db6909fee1fc3843
David <davidongaroh@gmail.com>
2022-10-01 23:33:16 +0300

    create serializers

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,6 +1,11 @@
 class RestaurantsController < ApplicationController
     def index
         restaurants = Restaurant.all
-        render json: restaurants
+        render json: restaurants, status: :ok
+    end
+
+    def show
+            restaurant = Restaurant.find(params[:id])
+            render json: restaurant, serializer: RestaurantAndPizzasSerializer, status: 200
     end
 end
