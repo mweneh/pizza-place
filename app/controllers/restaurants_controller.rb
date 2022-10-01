0cf86ca34f0594ac50fd046438d8ecf61a8eb021
David <davidongaroh@gmail.com>
2022-10-01 23:54:23 +0300

    add destroy dependnecy

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,4 +1,5 @@
 class RestaurantsController < ApplicationController
+rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
     def index
         restaurants = Restaurant.all
         render json: restaurants, status: :ok
@@ -7,5 +8,16 @@ class RestaurantsController < ApplicationController
     def show
             restaurant = Restaurant.find(params[:id])
             render json: restaurant, serializer: RestaurantAndPizzasSerializer, status: 200
+    end
+
+    def destroy
+        restaurant = Restaurant.find_by(id:params[:id])
+        restaurant.destroy
+        render json: {}, status: :no_content
+    end
+
+    private
+    def render_not_found_response
+        render json: {error:'Restaurant not found'}, status: :not_found
     end
 end
