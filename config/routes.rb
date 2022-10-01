cfbd152d1e5218676b8f666be42672424bd72d82
David <davidongaroh@gmail.com>
2022-10-01 23:19:05 +0300

    add specific routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :restaurants
+  resources :restaurants, only: [:index, :show, :destroy]
   resources :restaurant_pizzas
   resources :pizzas
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
