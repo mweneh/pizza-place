2f1a67a679403eae3d51a2f85d496e1dc0437247
David <davidongaroh@gmail.com>
2022-10-03 09:31:13 +0300

    add create, strong params and rescue errors

--- app/controllers/restaurant_pizzas_controller.rb
+++ app/controllers/restaurant_pizzas_controller.rb
@@ -1,2 +1,19 @@
 class RestaurantPizzasController < ApplicationController
+rescue_from ActiveRecord::RecordInvalid, with: :unprocessable_entity_response
+
+    def create
+        restaurant = RestaurantPizza.create!(pizza_params)
+        render json: restaurant.pizza, status: 201
+    end
+
+    private
+
+    def pizza_params
+        params.permit(:price, :restaurant_id, :pizza_id)
+    end
+
+    def unprocessable_entity_response(invalid)
+        render json:{error: invalid.record.errors}, status: 422
+    end
+
 end
